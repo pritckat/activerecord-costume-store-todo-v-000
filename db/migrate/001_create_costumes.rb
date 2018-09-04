4a359f8d9beefa4ebfc9b83c08e12d19958180ff
Katrina Pritchard <pritckat@gmail.com>
2018-09-04 18:59:32 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -5,6 +5,8 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       c.float :price
       c.string :size
       c.string :image_url
+      c.datetime :created_at
+      c.datetime :updated_at
     end
   end
 end
