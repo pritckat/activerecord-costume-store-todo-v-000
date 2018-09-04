e16380462caf3b6a5815366ebcc43159d49565fa
Katrina Pritchard <pritckat@gmail.com>
2018-09-04 19:05:46 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,13 @@
-# Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costume_stores do |cs|
+      cs.string :name
+      cs.string :location
+      cs.integer :costume_inventory
+      cs.integer :num_of_employees
+      cs.boolean :still_in_business
+      cs.datetime :opening_time
+      cs.datetime :closing_time
+    end
+  end
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,15 @@
-# Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+
+  def change
+    create_table :haunted_houses do |hh|
+      hh.string :name
+      hh.string :location
+      hh.string :theme
+      hh.float :price
+      hh.boolean :family_friendly
+      hh.datetime :opening_date
+      hh.datetime :closing_date
+      hh.text :description
+    end
+  end
+end
